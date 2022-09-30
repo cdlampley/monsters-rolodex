4de67f72fc55017dda5fdce5e9527b712c8adfb7
Cherie Lampley <cherielampley@Cheries-MacBook-Pro.local>
2022-09-29 21:03:06 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import { Component } from 'react'
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((users) => this.setState(() => {
+      return {monsters: users}
+    }))
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState(
+      () => {
+        return {searchField}
+    })
+  }
+
+  render() {
+
+    const {monsters, searchField} = this.state
+    const {onSearchChange} = this
+    
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+
+    return (
+      <div className="App">
+        <input className="search-box" type="search" placeholder="search monsters" onChange={onSearchChange} />
+        {filteredMonsters.map((monster) => {
+          return(
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default App;
