df3ea25a1a8c5a114ee7cba547b1ccfb87db046d
Cherie Lampley <cherielampley@Cheries-MacBook-Pro.local>
2022-10-01 09:27:26 -0400

    updated component files

--- src/App.js
+++ src/App.js
@@ -40,11 +40,11 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           className='monsters-search-box'
           placeholder='search monster'
-          onChangeHandler={onSearchChange}
-          
+          onChangeHandler={onSearchChange} 
         />
         <CardList monsters={filteredMonsters} />
       </div>
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,23 +1,15 @@
 import { Component } from 'react'
+import Card from './Card'
 
 class CardList extends Component {
 
     render() {
         const { monsters } = this.props
-
         return (
             <div className='card-list'>
                 {monsters.map((monster) => {
-                    const { name, email, id } = monster
-
                     return (
-                        <div className='card-container' key={id}>
-                            <img alt={`monster ${name}`}
-                                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                            />
-                            <h2>{name}</h2>
-                            <p>{email}</p>
-                        </div>
+                        <Card monster={monster} />
                     )
                 })}
             </div>
