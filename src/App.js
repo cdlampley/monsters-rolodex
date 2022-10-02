bb301780352855230b4db8efd89e22dbd937d91c
Cherie Lampley <cherielampley@Cheries-MacBook-Pro.local>
2022-10-02 10:58:45 -0400

    updated index.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Component } from 'react'
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
 
+
 class App extends Component {
   constructor() {
     super();
